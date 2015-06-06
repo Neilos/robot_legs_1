3e099e5017966f0be1def882635aabf9ec329a5a
Neil Atkinson <neil@new-bamboo.co.uk>
2015-06-06 13:28:23 +0100

    Add hover behaviour if hovering for a predetermined time.

--- javascript/dom_scripts.js
+++ javascript/dom_scripts.js
@@ -1,1 +1,15 @@
 $('body').prepend('<h1 id="hello">HELLO</h1>')
+
+var gazeTime = 1000;
+var timer;
+
+$('#hello').on({
+  mouseover: function () {
+    timer = setTimeout(function () {
+       alert("hello again")
+    }, gazeTime);
+  },
+  mouseout: function () {
+    clearTimeout(timer);
+  }
+});
