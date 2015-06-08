8fa72a8aa231a85e6c91b42481913e807e3c2277
Neil Atkinson <neil@new-bamboo.co.uk>
2015-06-08 17:44:20 +0100

    Prepare keyboard when user is about to click something.

--- javascript/dom_scripts.js
+++ javascript/dom_scripts.js
@@ -59,6 +59,7 @@ $('#eye-tracking-controls .action-button').on('click', function (e) {
     cursorPosition = { x: e.clientX, y: e.clientY }
   });
 
+  prepareKeyboard()
   startSamplingTargets()
   showCancelButton()
 })
@@ -97,6 +98,34 @@ function positionControls () {
   $('#eye-tracking-controls').css('top',offset);
   $('body').css('margin-bottom', controlsHeight);
   $('#eye-tracking-controls').css('display','block'); // show it once it's positioned
+}
+
+function prepareKeyboard () {
+  $('text, textarea, input[type!="submit"]').keyboard({
+    // set this to ISO 639-1 language code to override language set by the layout
+    // http://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
+    // language defaults to "en" if not found
+    language: ['en'], // string or array
+
+    // *** choose layout ***
+    layout: 'qwerty',
+    customLayout: { 'normal': ['{cancel}'] },
+
+    position : {
+      of : $('body'), // null (attach to input/textarea) or a jQuery object (attach elsewhere)
+      my : 'center top',
+      at : 'center top',
+      collision: 'fit fit'
+    },
+
+    reposition: true, // allow jQuery position utility to reposition the keyboard on window resize
+    usePreview: true, // preview added above keyboard if true, original input/textarea used if false
+    alwaysOpen: false, // if true, the keyboard will always be visible
+    initialFocus: true, // give the preview initial focus when the keyboard becomes visible
+    stayOpen: true, // if true, keyboard will remain open even if the input loses focus.
+    openOn: 'click', // Event (namespaced) on the input to reveal the keyboard. To disable it, just set it to ''.
+    keyBinding: 'click', // When the character is added to the input
+  })
 }
 
 function spotlightCursorMove (e) {
