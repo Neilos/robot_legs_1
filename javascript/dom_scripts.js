5693553a018c78f4cedbadc084677af1e85d7acd
Neil Atkinson <neil@new-bamboo.co.uk>
2015-06-09 01:45:50 +0100

    Zoom to make it easier to select.

--- javascript/dom_scripts.js
+++ javascript/dom_scripts.js
@@ -1,14 +1,56 @@
+
 var gazeTime = 1500 // time staring at something until action is confirmed
 var userSelectionDelay = 2500 // delay for user to focus before gazeTime starts
-var activationInterval = 100 // delay between target guesses
+var activationInterval = 500 // delay between target guesses
 var cursorSize = 90
-var hoverDuration = 400 // time to allow for hovering animations before triggering clicks
 var sampleSize = gazeTime / activationInterval // number of consecutive elements that should match
 var cursorPosition = { x: -1000, y: -1000 }
 var potentialTargets = []
 var actionActivationTimer = []
 var sampleIntervalTimer
 var actionButtonActivated = false
+var userZoomLevel = 1
+var newZoomLevel = userZoomLevel
+var zoomDelta = 0.1
+var animateZoom = true
+
+////////////////////////////////////
+
+var content = $('body')[0]
+var container = $('html')[0]
+var clientWidth = 0
+var clientHeight = 0
+var contentWidth = content.clientWidth
+var contentHeight = content.clientHeight
+
+var render = function(left, top, zoom) {
+  content.style.marginLeft = left ? (-left/zoom) + 'px' : '';
+  content.style.marginTop = top ? (-top/zoom) + 'px' : '';
+  content.style.zoom = zoom || '';
+};
+
+var scroller = new Scroller(render, {
+  zooming: true,
+  animating: true,
+  animationDuration: activationInterval
+});
+
+var rect = content.getBoundingClientRect();
+scroller.setPosition(rect.left + container.clientLeft, rect.top + container.clientTop);
+
+// Reflow handling
+var reflow = function() {
+  clientWidth = container.clientWidth;
+  clientHeight = container.clientHeight;
+  scroller.setDimensions(clientWidth, clientHeight, contentWidth, contentHeight);
+};
+
+window.addEventListener("resize", reflow, false);
+reflow();
+
+
+//////////////////////////////////
+
 
 var controls =  '<div id="eye-tracking-controls" style="position: fixed; display: none;">' +
                   '<div class="browser-controls" >' +
@@ -61,22 +103,21 @@ $('#eye-tracking-controls .action-button').on({
   }
 });
 
-function guessTarget (e) {
-  pickTarget(this)
-  e.stopPropagation()
-}
-
-function pickTarget (newTarget) {
-  potentialTargets.slice(-(sampleSize + 10))
-  potentialTargets.push(newTarget)
+function pickTarget (e, cursorX, cursorY) {
+  potentialTargets.slice(-(sampleSize))
+  potentialTargets.push(this)
   if (potentialTargets.length > sampleSize) {
     var recentTargetSamples = potentialTargets.slice(-sampleSize)
     var recentlySampledTargets = $.unique(recentTargetSamples)
-    if (recentlySampledTargets.length = 1) {
+    if (recentlySampledTargets.length == 1) {
       var targetElement = recentlySampledTargets[0]
-      triggerSelectionOf(targetElement)
+      triggerSelectionOf(targetElement, cursorX, cursorY)
+    } else {
+      newZoomLevel = newZoomLevel + zoomDelta
+      scroller.zoomTo(newZoomLevel, animateZoom, cursorX, cursorY);
     }
   }
+  e.stopPropagation()
 }
 
 function positionControls () {
@@ -100,8 +141,8 @@ function prepareKeyboard () {
 
     position : {
       of : $('body'), // null (attach to input/textarea) or a jQuery object (attach elsewhere)
-      my : 'center top',
-      at : 'center top',
+      my : 'left top',
+      at : 'left top',
       collision: 'fit fit'
     },
 
@@ -137,13 +178,13 @@ function spotlightCursorOff () {
 function sampleElements () {
   $('#eye-tracking-mask').css('pointer-events', 'none')
   var element = document.elementFromPoint(cursorPosition.x, cursorPosition.y)
-  $(element).trigger('eye-targeting')
+  $(element).trigger('eye-targeting', [ cursorPosition.x, cursorPosition.y ])
   $('#eye-tracking-mask').css('pointer-events', 'auto')
 }
 
 function startSamplingTargets () {
   potentialTargets = []
-  $('body *').on('eye-targeting', guessTarget)
+  $('body *').on('eye-targeting', pickTarget)
 }
 
 function stopSamplingTargets () {
@@ -151,10 +192,14 @@ function stopSamplingTargets () {
   potentialTargets = []
 }
 
-function triggerSelectionOf (element) {
-  try { $(element)[0].click() }
+function triggerSelectionOf (element, cursorX, cursorY) {
+  try {
+    $(element)[0].click()
+  }
   finally {
     console.log('Target element is: ', element)
+    newZoomLevel = userZoomLevel
+    scroller.zoomTo(userZoomLevel, animateZoom, cursorX, cursorY);
     stopSamplingTargets()
     spotlightCursorOff()
     actionButtonActivated = false
